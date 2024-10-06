df64ce80f002d09fad8bd793c3966d55e558d7cc
Christina Asipenka <krystinabutko@gmail.com>
2024-10-06 20:42:28 +0200

    create func

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
+import { existsSync, mkdirSync, writeFileSync } from 'node:fs';
+
+import { join } from 'path';
+const folderPath = join(process.cwd(), 'src/fs/files');
+const filePath = join(folderPath, 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+
+    if (existsSync(filePath)) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        const content = 'I am fresh and young!';
+        await writeFileSync(filePath, content);
+    } catch (err) {
+        throw new Error(err);
+    }
 };
 
-await create();
+try {
+    await create();
+} catch (err) {
+    console.log(err.message);
+}
