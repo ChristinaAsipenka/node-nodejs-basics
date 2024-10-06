cd5e5604f22a73abd62c4f515d88f1a092c99091
Christina Asipenka <krystinabutko@gmail.com>
2024-10-06 21:45:58 +0200

    read func

--- src/fs/read.js
+++ src/fs/read.js
@@ -15,7 +15,6 @@ const read = async () => {
 
 try {
     await read();
-    console.log('\'\x1b[32m%s\x1b[0m\'', 'File deleted successfully!')
 } catch (err) {
     console.log('\'\x1b[31m%s\x1b[0m\'', err.message)
 }
