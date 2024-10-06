82ed819c12396cb7be2d3ce6162342007f3160ad
Christina Asipenka <krystinabutko@gmail.com>
2024-10-06 22:26:26 +0200

    read func

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { createReadStream } from 'fs';
+const filePath = "src/streams/files/fileToRead.txt";
+
 const read = async () => {
-    // Write your code here 
+    const readStream = createReadStream(filePath);
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+
+    readStream.on('end', () => {
+        console.log('\nFinished reading file.');
+    });
+
+    readStream.on('error', (err) => {
+        console.error('Error reading file:', err.message);
+    });
 };
 
 await read();
