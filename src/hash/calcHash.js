92319ff25fcde15491d93eff590c68bbdeb7a18f
Christina Asipenka <krystinabutko@gmail.com>
2024-10-06 22:23:14 +0200

    hash func

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createHash } from 'crypto'; // Import the createHash function from the crypto module
+import { createReadStream } from 'fs'; // Import createReadStream from the fs module
+import { pipeline } from 'stream'; // Import the pipeline function from the stream module
+const filePath = 'src/hash/files/fileToCalculateHashFor.txt';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const readStream = createReadStream(filePath);
+    const hash = createHash('sha256');
+    pipeline(
+        readStream,
+        hash.setEncoding('hex'),
+        (err) => {
+            if (err) {
+                console.error('Error:', err.message);
+                return;
+            }
+            console.log('SHA256 Hash:', hash.read());
+        }
+    );
 };
 
 await calculateHash();
