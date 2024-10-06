29e62d6a67e7b2527f764005fc7548de563f6182
Christina Asipenka <krystinabutko@gmail.com>
2024-10-06 21:34:28 +0200

    delete func

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { existsSync, unlinkSync } from 'fs';
+import { join } from 'path';
+import { folderPath } from  './fsconst.js';
+const fileToRemovePath = join(folderPath, 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+    if (!existsSync(fileToRemovePath)) {
+        throw new Error('FS operation failed: fileToRemove.txt does not exist');
+    }
+
+    unlinkSync(fileToRemovePath);
 };
 
-await remove();
+try {
+    await remove();
+    console.log('\'\x1b[32m%s\x1b[0m\'', 'File deleted successfully!')
+} catch (err) {
+    console.log('\'\x1b[31m%s\x1b[0m\'', err.message)
+}
