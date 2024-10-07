d56e6616672b67cfd9a6fa727ed1f8c3bfe7ea42
Christina Asipenka <krystinabutko@gmail.com>
2024-10-07 20:24:59 +0200

    zip:compress func

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,30 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+
 const compress = async () => {
-    // Write your code here 
+    const inputFile = 'src/zip/files/fileToCompress.txt';
+    const outputFile = 'src/zip/files/archive.gz';
+
+    const readStream = createReadStream(inputFile);
+    const writeStream = createWriteStream(outputFile);
+    const gzip = createGzip();
+
+    pipeline(
+        readStream,
+        gzip,
+        writeStream,
+        (err) => {
+            if (err) {
+                throw new Error(err.message)
+            }
+        }
+    );
 };
 
-await compress();
+try {
+    await compress();
+    console.log(`File successfully compressed`);
+} catch (err) {
+    console.log(err.message)
+}
