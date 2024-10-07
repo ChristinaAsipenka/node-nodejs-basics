ac1fc0f90289c86211ed885994d102737e589b6c
Christina Asipenka <krystinabutko@gmail.com>
2024-10-07 21:13:15 +0200

    wt func

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -3,9 +3,17 @@ import { parentPort } from 'worker_threads';
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
+
     parentPort.on('message', (n) => {
-        const result = nthFibonacci(n);
-        parentPort.postMessage(result);
+        try {
+            console.log(`Received ${n} in worker thread`);
+            const result = nthFibonacci(n);
+            console.log(`Computed result: ${result}`);
+            parentPort.postMessage(result);
+        } catch (error) {
+            console.error('Error in worker thread:', error);
+            parentPort.postMessage({ error: error.message });
+        }
     });
 };
 
